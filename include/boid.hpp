cca3a2d38b36f70217715c68a616f70b14006fea
Anto <antogenovesi2510@gmail.com>
2026-09-04 18:02:49 +0200

    apply_force

--- include/boid.hpp
+++ include/boid.hpp
@@ -43,7 +43,9 @@ class Boid
   void apply_window_boundary(SimConfig const& config);
 
   void apply_force(Vector2D const& force_point, double influence_radius,
-                   double interaction_strength);
+                   double interaction_strength); // may be a risck to class
+                                                 // invariant (in update_physics
+                                                 // is performed after .update)
 
   void set_max_vel(double max_v);
   void set_min_vel(double min_v);
